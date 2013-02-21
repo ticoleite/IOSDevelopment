f757d19120fdae2ff67cd6ab4b4fadeac683e104
Thiago Bezerra <Tico@thiagomacbook.local>
2013-02-21 19:30:40 -0300

    Initial Commit

---
+++ BankAccount/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BankAccount
+//
+//  Created by Thiago Bezerra on 2/21/13.
+//  Copyright (c) 2013 Thiago Bezerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BankAccountAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BankAccountAppDelegate class]));
+    }
+}
